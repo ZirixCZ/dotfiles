e97b7863c172f891bf3e9c36c8a9877830d84625
Michal Vaniš <zirixcz@gmail.com>
2024-02-04 12:36:42 +0100

    update nvim config & add yabai config

--- config/nvim/lua/plugins/plugins.lua
+++ config/nvim/lua/plugins/plugins.lua
@@ -1,5 +1,5 @@
 return {
-  -- COLOR SCHEME
+  -- COLOR SCHEME/THEME
   {
     "catppuccin/nvim",
     name = "catppuccin",
@@ -51,9 +51,17 @@ return {
           -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
         },
       })
-      vim.cmd.colorscheme("catppuccin")
     end,
   },
+
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      transparent_mode = true,
+    },
+  },
+
+  -- NOTIFY
 
   {
     "rcarriga/nvim-notify",
@@ -178,30 +186,30 @@ return {
 
   -- DISCORD PRESENCE
   {
-    "andweeb/presence.nvim",
+    "SIGMazer/presence.nvim",
     config = function()
       require("presence").setup({
         -- General options
         auto_update = true, -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
-        neovim_image_text = ":3", -- Text displayed when hovered over the Neovim image
-        main_image = "file", -- Main image display (either "neovim" or "file")
+        neovim_image_text = "League of Legends", -- Text displayed when hovered over the Neovim image
+        main_image = "https://raw.githubusercontent.com/github/explore/b088bf18ff2af3f2216294ffb10f5a07eb55aa31/topics/league-of-legends/league-of-legends.png", -- Main image display (either "neovim" or "file")
         --client_id = "793271441293967371", -- Use your own Discord application client id (not recommended)
         log_level = nil, -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
         debounce_timeout = 10, -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
         enable_line_number = false, -- Displays the current line number instead of the current project
         blacklist = {}, -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
-        buttons = true, -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
+        buttons = false, -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
         file_assets = {}, -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
-        show_time = false, -- Show the timer
+        show_time = true, -- Show the timer
 
         -- Rich Presence text options
-        editing_text = "editing %s", -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
-        file_explorer_text = "browsing %s", -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
-        git_commit_text = "committing changes", -- Format string rendered when committing changes in git (either string or function(filename: string): string)
-        plugin_manager_text = "managing plugins", -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
-        reading_text = "reading %s", -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
-        workspace_text = "working on %s", -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
-        line_number_text = "line %s out of %s", -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
+        editing_text = "In Queue", -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
+        file_explorer_text = "In Queue", -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
+        git_commit_text = "In Queue", -- Format string rendered when committing changes in git (either string or function(filename: string): string)
+        plugin_manager_text = "In Queue", -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
+        reading_text = "In Queue", -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
+        workspace_text = "", -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
+        line_number_text = "",
       })
     end,
   },
@@ -228,5 +236,67 @@ return {
     "numToStr/Comment.nvim",
     opts = {},
     lazy = false,
+  },
+
+  -- HARPOON
+  {
+    "theprimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("harpoon"):setup()
+    end,
+    keys = {
+      {
+        "<leader>a",
+        function()
+          require("harpoon"):list():append()
+        end,
+        desc = "harpoon file",
+      },
+      {
+        "<leader>h",
+        function()
+          local harpoon = require("harpoon")
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "harpoon quick menu",
+      },
+      {
+        "<leader>1",
+        function()
+          require("harpoon"):list():select(1)
+        end,
+        desc = "harpoon to file 1",
+      },
+      {
+        "<leader>2",
+        function()
+          require("harpoon"):list():select(2)
+        end,
+        desc = "harpoon to file 2",
+      },
+      {
+        "<leader>3",
+        function()
+          require("harpoon"):list():select(3)
+        end,
+        desc = "harpoon to file 3",
+      },
+      {
+        "<leader>4",
+        function()
+          require("harpoon"):list():select(4)
+        end,
+        desc = "harpoon to file 4",
+      },
+      {
+        "<leader>5",
+        function()
+          require("harpoon"):list():select(5)
+        end,
+        desc = "harpoon to file 5",
+      },
+    },
   },
 }
